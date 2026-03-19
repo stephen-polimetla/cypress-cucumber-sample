2f87969031688f407858159d52cd60e33933df5e
stephen-polimetla <stephen.polimetla@gmail.com>
2026-03-19 13:24:27 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,8 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const path = require('path');
+const { execSync } = require('child_process');
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 /**
@@ -19,10 +21,26 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
+  
   on('before:browser:launch', (browser = {}, args) => {
     if (browser.family === 'chrome') {
       args.push('--disable-dev-shm-usage');
     }
     return args;
+  });
+
+  // Automatically generate the HTML report after the test run finishes
+  on('after:run', () => {
+    const reportScript = path.join(config.projectRoot, 'scripts', 'generate-cucumber-report.js');
+    try {
+      execSync(`node "${reportScript}"`, {
+        cwd: config.projectRoot,
+        stdio: 'inherit',
+      });
+    } catch (err) {
+      // Don't fail the whole test run if report generation fails
+      // eslint-disable-next-line no-console
+      console.error('Failed to generate cucumber HTML report:', err.message || err);
+    }
   });
 }
